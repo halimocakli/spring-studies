5e5cf8ad6acae4c237fe41badb6cfadb2328a3ae
halimocakli <halimcanocakli@gmail.com>
2024-09-03 12:47:40 +0300

    Feat: Build EazySchool Courses, About, Contact Pages & Intro to Lombok

---
+++ Example_21/src/main/java/com/halim/educativeschool/service/ContactService.java
@@ -0,0 +1,24 @@
+package com.halim.educativeschool.service;
+
+import com.halim.educativeschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ContactService
+{
+    /**
+     * Save Contact Details into DB
+     *
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact)
+    {
+        boolean isSaved = true;
+        //TODO - Need to persist the data into the DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+}
