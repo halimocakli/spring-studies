c5da10e5709e192450bd841d8e3b3dfaf58e783f
Halim OCAKLI <halimcanocakli@gmail.com>
2024-09-04 16:09:07 +0300

    Feat: Validating the form input using Java Bean & Hibernate Validators

---
+++ Example_24/src/main/java/com/halim/educativeschool/controller/ContactController.java
@@ -0,0 +1,47 @@
+package com.halim.educativeschool.controller;
+
+import com.halim.educativeschool.model.Contact;
+import com.halim.educativeschool.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Slf4j
+@Controller
+public class ContactController
+{
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService)
+    {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContactPage(Model model)
+    {
+        model.addAttribute("contact", new Contact());
+        return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors)
+    {
+        if (errors.hasErrors())
+        {
+            log.error(STR."Contact form validation failed due to: \{errors.toString()}");
+            return "contact.html";
+        }
+        contactService.saveMessageDetails(contact);
+        return "redirect:/contact";
+    }
+}
