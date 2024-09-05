05d425a45d511dff190934249a69ad30ea757613
halimocakli <halimcanocakli@gmail.com>
2024-09-05 11:44:11 +0300

    Revert "Feat: Hello World Web App using Thymeleaf & DevTools Demo"
    This reverts commit 7224dfe13dd627d33cda7bfe5041b13ae4c866f4.

--- Example_24/src/main/java/com/halim/educativeschool/controller/ContactController.java
+++ Example_24/src/main/java/com/halim/educativeschool/controller/ContactController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -37,7 +38,7 @@ public class ContactController
     {
         if (errors.hasErrors())
         {
-            log.error("Contact form validation failed due to : {}", errors.toString());
+            log.error(STR."Contact form validation failed due to: \{errors.toString()}");
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
